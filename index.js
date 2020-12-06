5205c13243e466cdbaeaaa411f0c5486ae4f6f22
Dominik Eitler <eitler.domi@gmail.com>
2020-12-06 14:49:28 +0100

    added slider function #4

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const height = window.innerHeight - 50;
+const height = window.innerHeight - 150;
 const width = window.innerWidth - 200;
+const margin = { right: 50, left: 50 };
 
 const boundaryFilter = (a, b) => a !== b;
 const textOffset = { x: 0, y: 3 };
 const greys = d3.schemeGreys[9];
 
 const getColor = (oxygen, min, max) => `hsl(240, ${(oxygen - min) / (max - min) * 100}%, 50%)`;
+const compareDates = (date1, date2) => date1.getMonth() == date2.getMonth() && date1.getYear() == date2.getYear();
 
 // import data
 d3.json(
@@ -73,15 +75,28 @@ d3.json(
 
 
         d3.csv("oxygen.csv")
-            .row(r => ({ id: r['Station_Number'], lat: +r['Lat'], long: +r['Lng'], oxygen: +r['Oxygen'], year: +r['year'], month: +r['month'] }))
+            .row(r => ({
+                id: r['Station_Number'],
+                lat: +r['Lat'],
+                long: +r['Lng'],
+                oxygen: +r['Oxygen'],
+                year: +r['year'],
+                month: +r['month'],
+                date: new Date(+r['year'], +r['month'])
+            }))
             .get((error, rows) => {
 
-                let markers = rows.filter((r) => (r.year === 2000 && r.month === 5));
+                const maxDate = new Date(Math.max.apply(null, rows.map(r => r.date)));
+                const minDate = new Date(Math.min.apply(null, rows.map(r => r.date)));
+
+                const startDate = minDate;
+
                 const maxOxygen = Math.max(...rows.map(m => m.oxygen));
                 const minOxygen = Math.min(...rows.map(m => m.oxygen));
 
+
                 // construct svg
-                var svg = d3
+                const mapSvg = d3
                     .select('#map')
                     .append('svg')
                     .attr('width', width)
@@ -89,7 +104,7 @@ d3.json(
                     .call(zoom);
 
                 // map layer group
-                const g = svg.append('g').attr('id', 'map-layers');
+                const g = mapSvg.append('g').attr('id', 'map-layers');
 
                 // background
                 g.append('rect')
@@ -119,8 +134,8 @@ d3.json(
                     .enter()
                     .append('circle')
                     .classed('places', true)
-                    .attr('cx', (d) => path.centroid(d)[0])
-                    .attr('cy', (d) => path.centroid(d)[1])
+                    .attr('cx', d => path.centroid(d)[0])
+                    .attr('cy', d => path.centroid(d)[1])
                     .attr('r', 1.5)
                     .attr('fill', 'white')
                     .attr('stroke', greys[5])
@@ -138,15 +153,15 @@ d3.json(
                     .enter()
                     .append('text')
                     .classed('place-label', true)
-                    .attr('x', (d) => path.centroid(d)[0])
-                    .attr('y', (d) => path.centroid(d)[1] - textOffset.y)
+                    .attr('x', d => path.centroid(d)[0])
+                    .attr('y', d => path.centroid(d)[1] - textOffset.y)
                     .attr('text-anchor', 'end')
                     .attr('fill', greys[7])
                     .style('font', '7px sans-serif')
-                    .text((d) => d.properties.name);
+                    .text(d => d.properties.name);
 
                 // tooltip
-                var Tooltip = d3
+                const Tooltip = d3
                     .select('#map')
                     .append('div')
                     .attr('class', 'tooltip')
@@ -159,33 +174,102 @@ d3.json(
                     .style('padding', '5px');
 
                 // tooltip hover function
-                var mouseover = function (d) {
+                const mouseover = d => {
                     Tooltip.style('visibility', 'visible');
                 };
-                var mousemove = function (d) {
+                const mousemove = d => {
                     Tooltip.html(
                         d.id + '<br>' + 'long: ' + d.long + '<br>' + 'lat: ' + d.lat
                     )
                         .style('left', d3.mouse(this)[0] + 10 + 'px')
                         .style('top', d3.mouse(this)[1] + 2 + 'px');
                 };
-                var mouseleave = function (d) {
+                const mouseleave = d => {
                     Tooltip.style('visibility', 'hidden');
                 };
 
-                // circles
-                g.selectAll('myCircles')
-                    .data(markers)
-                    .enter()
-                    .append('circle')
-                    .attr('cx', (d) => projection([d.long, d.lat])[0])
-                    .attr('cy', (d) => projection([d.long, d.lat])[1])
-                    .attr('r', 10)
-                    .style('fill', (d) => getColor(d.oxygen, minOxygen, maxOxygen))
-                    .attr('fill-opacity', 0.9)
-                    .on('mouseover', mouseover)
-                    .on('mousemove', mousemove)
-                    .on('mouseleave', mouseleave);
+
+                /* ==================================================== */
+                /*                        BUBBLES                       */
+                /* ==================================================== */
+
+                const updateBubbles = date => {
+                    let markers = rows.filter(r => compareDates(r.date, date));
+
+                    g.selectAll("circle").remove();
+
+                    g.selectAll('myCircles')
+                        .data(markers)
+                        .enter()
+                        .append('circle')
+                        .attr('cx', d => projection([d.long, d.lat])[0])
+                        .attr('cy', d => projection([d.long, d.lat])[1])
+                        .attr('r', 10)
+                        .style('fill', d => getColor(d.oxygen, minOxygen, maxOxygen))
+                        .attr('fill-opacity', 0.9)
+                        .on('mouseover', mouseover)
+                        .on('mousemove', mousemove)
+                        .on('mouseleave', mouseleave);
+                }
+
+                updateBubbles(startDate);
+
+
+                /* ==================================================== */
+                /*                        SLIDER                        */
+                /* ==================================================== */
+
+                formatDate = d3.timeFormat("%b %y");
+
+                const sliderSvg = d3.select("#svg"),
+                    sliderWidth = width - margin.left - margin.right,
+                    sliderHeight = 100;
+
+                // scale function
+                const x = d3.scaleTime()
+                    .domain([minDate, maxDate])
+                    .range([0, sliderWidth])
+                    .clamp(true);
+
+                // initial value
+                const startValue = x(startDate);
+                startingValue = startDate;
+
+                const slider = sliderSvg.append("g")
+                    .attr("class", "slider")
+                    .attr("transform", "translate(" + margin.left + "," + (sliderHeight - 60) + ")");
+
+                slider.append("line")
+                    .attr("class", "track")
+                    .attr("x1", x.range()[0])
+                    .attr("x2", x.range()[1])
+                    .select(function () { return this.parentNode.appendChild(this.cloneNode(true)); })
+                    .attr("class", "track-inset")
+                    .select(function () { return this.parentNode.appendChild(this.cloneNode(true)); })
+                    .attr("class", "track-overlay")
+                    .call(d3.drag()
+                        .on("start.interrupt", () => slider.interrupt())
+                        .on("start drag", () => updateData(x.invert(d3.event.x))));
+
+                slider.insert("g", ".track-overlay")
+                    .attr("class", "ticks")
+                    .attr("transform", "translate(0," + 18 + ")")
+                    .selectAll("text")
+                    .data(x.ticks(10))
+                    .enter().append("text")
+                    .attr("x", x)
+                    .attr("text-anchor", "middle")
+                    .text(d => formatDate(d));
+
+                const handle = slider.insert("circle", ".track-overlay")
+                    .attr("class", "handle")
+                    .attr("r", 10);
+
+                const updateData = h => {
+                    handle.attr("cx", x(h));
+                    console.log(formatDate(h))
+                    updateBubbles(h);
+                }
 
             });
 
